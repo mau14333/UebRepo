1bd0dcd996605111905a67c619848b7c69b97ef7
Michael Maurer <maurerm3141@gmail.com>
2014-02-21 09:57:27 +0100

    Extracted askname method

--- TheSecond.java
+++ TheSecond.java
@@ -1,15 +1,10 @@
 public class TheSecond{
-	public static void main(String[] args)
+	public static String[] askname(String first, String last)
 	{
-		String first;
-		String last;
-
 		System.out.print("Enter your first name: ");
 		first = System.console().readLine();
-
 		System.out.print("Now enter your last name: ");
 		last = System.console().readLine();
-		
-		System.out.println("Hello " + first + " " + last);
+		return new String[]{first, last};
 	}
 }
